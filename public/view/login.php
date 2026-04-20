eb0855ca10e4ec092539f90b9090b954a750e3fb
chrisshuo123 <chr1z100@gmail.com>
2026-04-20 21:44:18 +0700

    refine the session by disabling the session_start() where it already declares in the src/Services/SessionManager.php, and refining by adding the  ternary checking incase user logins with wrong user or password

---
+++ public/view/login.php
@@ -0,0 +1,89 @@
+<?php
+// Session already handled in the src.
+// session_start();
+// if (isset($_SESSION['errors'])) {
+//   $errors = $_SESSION['errors'] ?? [];
+//   unset($_SESSION['errors']);
+// }
+
+$errors = $_SESSION['errors'] ?? [];
+unset($_SESSION['errors']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register & Login</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="/bikin-register-login-php/public/css/style.css">
+</head>
+
+<body>
+
+  <div class="container" id="signIn">
+    <h1 class="form-title">Sign In</h1>
+    <?php
+    if (isset($errors['login'])) {
+      echo '<div class="error-main">
+                <p>' . $errors['login'] . '</p>
+            </div>';
+      unset($errors['login']);
+    }
+    ?>
+    <form method="POST" action="/bikin-register-login-php/index.php">
+      <!-- Input Email -->
+      <div class="input-group">
+        <i class="fas fa-envelope"></i>
+        <input type="email" name="email" id="email" placeholder="Email" required>
+        <?php
+        if (isset($errors['email'])) {
+          echo ' <div class="error">
+                    <p>' . $errors['email'] . '</p>
+                </div>';
+        }
+        ?>
+      </div>
+      <!-- Input Password -->
+      <div class="input-group password">
+        <i class="fas fa-lock"></i>
+        <input type="password" name="password" id="password" placeholder="Password" required>
+        <i id="eye" class="fa fa-eye"></i>
+        <?php
+        if (isset($errors['password'])) {
+          echo ' <div class="error">
+                    <p>' . $errors['password'] . '</p>
+                </div>';
+        }
+        ?>
+      </div>
+      <!-- Recover Password -->
+      <p class="recover">
+        <a href="#">Recover Password</a>
+      </p>
+      <input type="submit" class="btn" value="Sign In" name="signin">
+    </form>
+    <p class="or">
+      ----------or--------
+    </p>
+    <div class="icons">
+      <i class="fab fa-google"></i>
+      <i class="fab fa-facebook"></i>
+    </div>
+    <!-- Sign Up Button -->
+    <div class="links">
+      <p>Don't have account yet?</p>
+      <a href="/bikin-register-login-php/public/view/register.php">Sign Up</a>
+    </div>
+  </div>
+  <script src="bikin-register-login-php/public/js/script.js"></script>
+</body>
+
+</html>
+<?php
+if (isset($_SESSION['errors'])) {
+  unset($_SESSION['errors']);
+}
+?>
